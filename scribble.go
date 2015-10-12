fdc13448a7fc46e83fe7c7280b8d6a85ab14b756
Steve Domino <domino.steve@gmail.com>
2015-10-12 12:05:06 -0600

    missed merge confilct corrected

--- scribble.go
+++ scribble.go
@@ -119,11 +119,7 @@ func (d *Driver) write(trans Transaction) error {
 	tmpPath := finalPath + "~"
 
 	// write marshaled data to a file, named by the resourceID
-<<<<<<< Updated upstream
-	if err := ioutil.WriteFile(tmpPath, b, 0666); err != nil {
-=======
-	if err := ioutil.WriteFile(dir+"/"+trans.ResourceID, b, 0644); err != nil {
->>>>>>> Stashed changes
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
 		return err
 	}
 
