f6d7310e91748ff6b9c5b4f006405872435e115b
Steve Domino <domino.steve@gmail.com>
2015-11-15 23:44:00 -0700

    updated some tests which uncovered some issues

--- scribble.go
+++ scribble.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 )
 
@@ -48,8 +47,6 @@ func New(dir string, logger Logger) (driver *Driver, err error) {
 		logger = lumber.NewConsoleLogger(lumber.INFO)
 	}
 
-	logger.Info("Creating scribble database at '%v'...\n", dir)
-
 	//
 	driver = &Driver{
 		dir:     dir,
@@ -57,68 +54,25 @@ func New(dir string, logger Logger) (driver *Driver, err error) {
 		log:     logger,
 	}
 
+	logger.Info("Creating scribble database at '%v'...\n", dir)
+
 	// create database
-	return driver, mkDir(dir)
-}
-
-// Read a record from the database
-func (d *Driver) Read(collection, resource string, v interface{}) error {
-
-	//
-	path := filepath.Join(collection, resource)
-	dir := filepath.Join(d.dir, path)
-
-	//
-	switch fi, err := stat(dir); {
-
-	// if fi is nil or error is not nil return
-	case fi == nil, err != nil:
-		return fmt.Errorf("Unable to find file or directory named %v\n", path)
-
-	// if the path is a directory, attempt to read all entries into v
-	case fi.Mode().IsDir():
-
-		// read all the files in the transaction.Collection; an error here just means
-		// the collection is either empty or doesn't exist
-		files, _ := ioutil.ReadDir(dir)
-
-		// the files read from the database
-		var f []string
-
-		// iterate over each of the files, attempting to read the file. If successful
-		// append the files to the collection of read files
-		for _, file := range files {
-			b, err := ioutil.ReadFile(filepath.Join(dir, file.Name()))
-			if err != nil {
-				return err
-			}
-
-			// append read file
-			f = append(f, string(b))
-		}
-
-		// unmarhsal the read files as a comma delimeted byte array
-		return json.Unmarshal([]byte("["+strings.Join(f, ",")+"]"), v)
-
-		// if the path is a file, attempt to read the single file
-	case fi.Mode().IsRegular():
-
-		// read record from database
-		b, err := ioutil.ReadFile(dir + ".json")
-		if err != nil {
-			return err
-		}
-
-		// unmarshal data
-		return json.Unmarshal(b, &v)
-	}
-
-	return nil
+	return driver, os.MkdirAll(dir, 0755)
 }
 
 // Write locks the database and attempts to write the record to the database under
 // the [collection] specified with the [resource] name given
 func (d *Driver) Write(collection, resource string, v interface{}) error {
+
+	// ensure there is a place to save record
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!")
+	}
+
+	// ensure there is a resource (name) to save record as
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)!")
+	}
 
 	mutex := d.getOrCreateMutex(collection)
 	mutex.Lock()
@@ -126,6 +80,13 @@ func (d *Driver) Write(collection, resource string, v interface{}) error {
 
 	//
 	dir := filepath.Join(d.dir, collection)
+	fnlPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnlPath + ".tmp"
+
+	// create collection directory
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
 
 	//
 	b, err := json.MarshalIndent(v, "", "\t")
@@ -133,21 +94,84 @@ func (d *Driver) Write(collection, resource string, v interface{}) error {
 		return err
 	}
 
-	// create collection directory
-	if err := mkDir(dir); err != nil {
-		return err
-	}
-
-	finalPath := filepath.Join(dir, resource+".json")
-	tmpPath := finalPath + "~"
-
 	// write marshaled data to the temp file
 	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
 		return err
 	}
 
 	// move final file into place
-	return os.Rename(tmpPath, finalPath)
+	return os.Rename(tmpPath, fnlPath)
+}
+
+// Read a record from the database
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+
+	// ensure there is a place to save record
+	if collection == "" {
+		return fmt.Errorf("Missing collection - no place to save record!")
+	}
+
+	// ensure there is a resource (name) to save record as
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record (no name)!")
+	}
+
+	//
+	record := filepath.Join(d.dir, collection, resource)
+
+	// check to see if file exists
+	if _, err := stat(record); err != nil {
+		return err
+	}
+
+	// read record from database
+	b, err := ioutil.ReadFile(record + ".json")
+	if err != nil {
+		return err
+	}
+
+	// unmarshal data
+	return json.Unmarshal(b, &v)
+}
+
+// ReadAll records from a collection; this is returned as a slice of strings because
+// there is no way of knowing what type they record is
+func (d *Driver) ReadAll(collection string) ([]string, error) {
+
+	// ensure there is a collection to read
+	if collection == "" {
+		return nil, fmt.Errorf("Missing collection - unable to record location!")
+	}
+
+	//
+	dir := filepath.Join(d.dir, collection)
+
+	// check to see if collection (directory) exists
+	if _, err := stat(dir); err != nil {
+		return nil, err
+	}
+
+	// read all the files in the transaction.Collection; an error here just means
+	// the collection is either empty or doesn't exist
+	files, _ := ioutil.ReadDir(dir)
+
+	// the files read from the database
+	var records []string
+
+	// iterate over each of the files, attempting to read the file. If successful
+	// append the files to the collection of read files
+	for _, file := range files {
+		b, err := ioutil.ReadFile(filepath.Join(dir, file.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		// append read file
+		records = append(records, string(b))
+	}
+
+	// unmarhsal the read files as a comma delimeted byte array
+	return records, nil
 }
 
 // Delete locks that database and then attempts to remove the collection/resource
@@ -207,16 +231,4 @@ func (d *Driver) getOrCreateMutex(collection string) sync.Mutex {
 	}
 
 	return m
-}
-
-// mkDir is a simple wrapper that attempts to make a directory at a specified
-// location
-func mkDir(d string) (err error) {
-
-	//
-	if _, err = os.Stat(d); err != nil {
-		return
-	}
-
-	return os.MkdirAll(d, 0755)
 }
